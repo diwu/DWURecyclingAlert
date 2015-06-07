5e8dc6360805e1815f6d274afad644a7ec45925a
Di Wu <diwufet@163.com>
2015-06-07 21:22:15 +0800

    implemented non recycled view object detection logic

--- RecyclingAlert/DWURecyclingAlert/DWURecyclingAlert.m
+++ RecyclingAlert/DWURecyclingAlert/DWURecyclingAlert.m
@@ -10,11 +10,13 @@
 #import <objc/runtime.h>
 #import <objc/message.h>
 #import <UIKit/UITableViewCell.h>
+#import "UIView+DWURecyclingAlert.h"
+#import <QuartzCore/CALayer.h>
 
 #define DWU_PROPERTY(propName) NSStringFromSelector(@selector(propName))
 
 // http://www.mikeash.com/pyblog/friday-qa-2010-01-29-method-replacement-for-fun-and-profit.html
-static BOOL duw_replaceMethodWithBlock(Class c, SEL origSEL, SEL newSEL, id block) {
+static BOOL dwu_replaceMethodWithBlock(Class c, SEL origSEL, SEL newSEL, id block) {
     if ([c instancesRespondToSelector:newSEL]) return YES; // Selector already implemented, skip silently.
     
     Method origMethod = class_getInstanceMethod(c, origSEL);
@@ -36,28 +38,47 @@ static BOOL duw_replaceMethodWithBlock(Class c, SEL origSEL, SEL newSEL, id bloc
     return YES;
 }
 
-static void dwu_recyclingAlert(UITableViewCell *_self) {
-    NSLog(@"Swizzled: %@", _self);
+static void dwu_markAllSubviewsAsRecycled(UITableViewCell *_self) {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        for (UIView *view in _self.contentView.subviews) {
+            view.dwuRecyclingCount = @(1);
+        }
+    });
+}
+
+static void dwu_checkNonRecycledSubviews(UITableViewCell *_self) {
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        for (UIView *view in _self.contentView.subviews) {
+            NSNumber *recyclingCount = view.dwuRecyclingCount;
+            if (!recyclingCount) {
+                view.dwuRecyclingCount = @(1);
+                view.layer.borderColor = [[UIColor redColor] CGColor];
+                view.layer.borderWidth = 5.0;
+            } else {
+                view.layer.borderColor = [[UIColor clearColor] CGColor];
+                view.layer.borderWidth = 0.0;
+            }
+        }
+    });
 }
 
 #if DEBUG
 __attribute__((constructor)) static void DWURecyclingAlert(void) {
     @autoreleasepool {
-        for (NSString *selStr in @[DWU_PROPERTY(setNeedsLayout), DWU_PROPERTY(setNeedsDisplay), DWU_PROPERTY(setNeedsDisplayInRect:)]) {
-            SEL selector = NSSelectorFromString(selStr);
-            SEL newSelector = NSSelectorFromString([NSString stringWithFormat:@"dwu_%@", selStr]);
-            if ([selStr hasSuffix:@":"]) {
-                duw_replaceMethodWithBlock(UITableViewCell.class, selector, newSelector, ^(__unsafe_unretained UITableViewCell *_self, CGRect r) {
-                    dwu_recyclingAlert(_self);
-                    ((void ( *)(id, SEL, CGRect))objc_msgSend)(_self, newSelector, r);
-                });
-            }else {
-                duw_replaceMethodWithBlock(UITableViewCell.class, selector, newSelector, ^(__unsafe_unretained UITableViewCell *_self) {
-                    dwu_recyclingAlert(_self);
-                    ((void ( *)(id, SEL))objc_msgSend)(_self, newSelector);
-                });
-            }
-        }
+        NSString *selStr = DWU_PROPERTY(prepareForReuse);
+        SEL selector = NSSelectorFromString(selStr);
+        SEL newSelector = NSSelectorFromString([NSString stringWithFormat:@"dwu_%@", selStr]);
+        dwu_replaceMethodWithBlock(UITableViewCell.class, selector, newSelector, ^(__unsafe_unretained UITableViewCell *_self) {
+            ((void ( *)(id, SEL))objc_msgSend)(_self, newSelector);
+            dwu_checkNonRecycledSubviews(_self);
+        });
+        selStr = DWU_PROPERTY(initWithStyle:reuseIdentifier:);
+        selector = NSSelectorFromString(selStr);
+        newSelector = NSSelectorFromString([NSString stringWithFormat:@"dwu_%@", selStr]);
+        dwu_replaceMethodWithBlock(UITableViewCell.class, selector, newSelector, (id)^(__unsafe_unretained UITableViewCell *_self, NSInteger arg1, __unsafe_unretained id arg2) {
+            dwu_markAllSubviewsAsRecycled(_self);
+            return ((id ( *)(id, SEL, NSInteger, id))objc_msgSend)(_self, newSelector, arg1, arg2);
+        });
     }
 }
 #endif
