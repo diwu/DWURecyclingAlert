36f9055712c6c45c8edbe7ca17e2958498521973
Di Wu <diwufet@163.com>
2015-06-07 11:12:53 +0800

    Initial Commit

---
+++ RecyclingAlert/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecyclingAlert
+//
+//  Created by Di Wu on 6/7/15.
+//  Copyright (c) 2015 Di Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecyclingAlert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RecyclingAlert
+//
+//  Created by Di Wu on 6/7/15.
+//  Copyright (c) 2015 Di Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
