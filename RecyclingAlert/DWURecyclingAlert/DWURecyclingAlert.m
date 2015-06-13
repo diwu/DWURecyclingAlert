04a643a1f6c4b98002d4b86e97bea7604bd36c60
Di Wu <diwufet@163.com>
2015-06-13 18:42:13 +0800

    code refactor

--- RecyclingAlert/DWURecyclingAlert/DWURecyclingAlert.m
+++ RecyclingAlert/DWURecyclingAlert/DWURecyclingAlert.m
@@ -11,10 +11,32 @@
 #import <objc/message.h>
 #import <UIKit/UITableViewCell.h>
 #import <UIKit/UIImage.h>
-#import "UIView+DWURecyclingAlert.h"
 #import <QuartzCore/CALayer.h>
 
+#define DWURecyclingAlertEnabled
+
 #define DWU_PROPERTY(propName) NSStringFromSelector(@selector(propName))
+
+#if defined (DEBUG) && defined (DWURecyclingAlertEnabled)
+@interface UIView (DWURecyclingAlert)
+
+@property (nonatomic, strong) NSNumber *dwuRecyclingCount;
+
+@end
+
+@implementation UIView (DWURecyclingAlert)
+
+@dynamic dwuRecyclingCount;
+
+- (void)setDwuRecyclingCount:(NSNumber *)number {
+    objc_setAssociatedObject(self, @selector(dwuRecyclingCount), number, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (id)dwuRecyclingCount {
+    return objc_getAssociatedObject(self, @selector(dwuRecyclingCount));
+}
+
+@end
 
 // http://www.mikeash.com/pyblog/friday-qa-2010-01-29-method-replacement-for-fun-and-profit.html
 static BOOL dwu_replaceMethodWithBlock(Class c, SEL origSEL, SEL newSEL, id block) {
@@ -92,28 +114,6 @@ static void dwu_checkNonRecycledSubviews(UITableViewCell *_self) {
     });
 }
 
-//static void dwu_recursionHelper3(UIView *view) {
-//    NSNumber *recyclingCount = view.dwuRecyclingCount;
-//    if (!recyclingCount) {
-//        view.dwuRecyclingCount = @(1);
-//        view.layer.borderColor = [[UIColor redColor] CGColor];
-//        view.layer.borderWidth = 5.0;
-//    } else {
-//        view.layer.borderColor = [[UIColor clearColor] CGColor];
-//        view.layer.borderWidth = 0.0;
-//    }
-//    for (UIView *subview in view.subviews) {
-//        dwu_recursionHelper2(subview);
-//    }
-//}
-//
-//static void dwu_checkNonRecycledImagesInSubviews(UITableViewCell *_self) {
-//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-//        dwu_recursionHelper3(_self.contentView);
-//    });
-//}
-
-#if DEBUG
 __attribute__((constructor)) static void DWURecyclingAlert(void) {
     @autoreleasepool {
         NSString *selStr = DWU_PROPERTY(prepareForReuse);
@@ -130,12 +130,6 @@ __attribute__((constructor)) static void DWURecyclingAlert(void) {
             dwu_markAllSubviewsAsRecycled(_self);
             return ((id ( *)(id, SEL, NSInteger, id))objc_msgSend)(_self, newSelector, arg1, arg2);
         });
-//        selStr = DWU_PROPERTY(setImage:);
-//        selector = NSSelectorFromString(selStr);
-//        newSelector = NSSelectorFromString([NSString stringWithFormat:@"dwu_%@", selStr]);
-//        dwu_replaceMethodWithBlock(UITableViewCell.class, selector, newSelector, ^(__unsafe_unretained UITableViewCell *_self, __unsafe_unretained id arg) {
-//            ((void ( *)(id, SEL, id))objc_msgSend)(_self, newSelector, arg);
-//        });
     }
 }
 #endif
